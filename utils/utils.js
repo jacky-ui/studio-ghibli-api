08e78aa26bf1b0b0d423542cca05a0a40cf8b153
jacky-ui <jacky.du.004@gmail.com>
2023-02-14 20:19:37 -0500

    create function in utils.js for GET requests

---
+++ utils/utils.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+// Function to read data.json for GET requests
+function readMovieData() {
+    const movieData = fs.readFileSync("../assets/data.json");
+    const parsedMovieData = JSON.parse(movieData);
+    return parsedMovieData;
+};
+
