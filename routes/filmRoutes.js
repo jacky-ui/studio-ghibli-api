1658cf5f1d402a4586e412cdbe0e1f4eca9860d0
jacky-ui <jacky.du.004@gmail.com>
2023-02-15 20:55:38 -0500

    made function to handle get request in filmRoutes. create dummy text to send over for get request. tested with postman and works

--- routes/filmRoutes.js
+++ routes/filmRoutes.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const utils = require('../utils/utils');
+
+router.get('/', (req, res) => {
+   res.status(200).send("Testing sent"); 
+});
+
+module.exports = router;
