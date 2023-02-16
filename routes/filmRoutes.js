1c002c19690f6abcb70977b9a5ca5a82974255c4
jacky-ui <jacky.du.004@gmail.com>
2023-02-15 21:25:51 -0500

    create testing if statement that will print out not found if id is undefined

--- routes/filmRoutes.js
+++ routes/filmRoutes.js
@@ -13,6 +13,10 @@ router.get("/:filmId", (req, res) => {
     const allFilms = utils.readMovieData();
     const selectedMovie = allFilms.find((film) => film.id === req.params.filmId);
     console.log(selectedMovie);
+
+    if( selectedMovie === undefined) {
+        console.log("not found")
+    } else
     res.status(200).send("Specific film");
 });
 
