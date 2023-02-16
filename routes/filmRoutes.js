0cb9006da3dfae8fc60e78f4089811d58787b930
jacky-ui <jacky.du.004@gmail.com>
2023-02-15 21:18:45 -0500

    create test get route that will console.log req.params when get request is made for specific film end point. tested and works with postman

--- routes/filmRoutes.js
+++ routes/filmRoutes.js
@@ -5,7 +5,12 @@ const utils = require('../utils/utils');
 // GET list of all films
 router.get('/', (req, res) => {
     const allFilms = utils.readMovieData();
-   res.status(200).send(allFilms); 
+    res.status(200).send(allFilms); 
+});
+
+router.get("/:filmId", (req, res) => {
+    console.log(req.params.filmId);
+    res.status(200).send("Specific film");
 });
 
 module.exports = router;
