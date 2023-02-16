68deed71bceef1f28eac542142efa07c9f8a76b2
jacky-ui <jacky.du.004@gmail.com>
2023-02-15 20:59:05 -0500

    replace dummy test on res with data.json upon get request to /film. also noticed that I didn't set the path for data properly in utils.js

--- routes/filmRoutes.js
+++ routes/filmRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const utils = require('../utils/utils');
 
 router.get('/', (req, res) => {
-   res.status(200).send("Testing sent"); 
+    const allFilms = utils.readMovieData();
+   res.status(200).send(allFilms); 
 });
 
 module.exports = router;
