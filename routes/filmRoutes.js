87070e75c5c888f0268b2d7132fb2c35d5a54fa5
jacky-ui <jacky.du.004@gmail.com>
2023-02-15 21:00:50 -0500

    add comment on filmRoutes function

--- routes/filmRoutes.js
+++ routes/filmRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const utils = require('../utils/utils');
 
+// GET list of all films
 router.get('/', (req, res) => {
     const allFilms = utils.readMovieData();
    res.status(200).send(allFilms); 
