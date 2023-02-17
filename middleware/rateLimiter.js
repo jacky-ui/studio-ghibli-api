35da647e018f8e1e2f629670de245e02f2c9f50b
jacky-ui <jacky.du.004@gmail.com>
2023-02-17 18:41:22 -0500

    change max request limit

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const rateLimiterUsingThirdParty = rateLimit({
   windowMs: 15 * 60 * 60 * 1000, // 24 hrs in milliseconds
-  max: 2,
+  max: 10,
   message: 'You have exceeded the 100 requests in 24 hrs limit!', 
   standardHeaders: true,
   legacyHeaders: false,
