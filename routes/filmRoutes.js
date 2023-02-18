83c7367d492acd009a40f00a7fcb36b6c86c8d79
jacky-ui <jacky.du.004@gmail.com>
2023-02-17 20:21:40 -0500

    notice that i am not dry when I am comparing/finding id of movie, so i create a function

--- routes/filmRoutes.js
+++ routes/filmRoutes.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const utils = require('../utils/utils');
 
+function grabMovieById(reqParamsId) {
+    const allFilms = utils.readMovieData();
+    const selectedMovie = allFilms.find((film) => film.id === reqParamsId);
+    return selectedMovie;
+};
+
 // GET list of all films
 router.get('/', (req, res) => {
     const allFilms = utils.readMovieData();
@@ -18,9 +24,10 @@ router.get("/:filmId", (req, res) => {
 
 // GET film poster/image based on id provided from request
 router.get("/:filmId/poster", (req,res) => {
-    const allFilms = utils.readMovieData();
-    const selectedMovie = allFilms.find((film) => film.id === req.params.filmId);
-    selectedMovie ? res.status(200).send(selectedMovie.poster) : res.status(404).send("Not found. Please double check URL endpoint");
+    console.log(grabMovieById(req.params.filmId));
+    // // const allFilms = utils.readMovieData();
+    // // const selectedMovie = allFilms.find((film) => film.id === req.params.filmId);
+    // selectedMovie ? res.status(200).send(selectedMovie.poster) : res.status(404).send("Not found. Please double check URL endpoint");
 });
 
 module.exports = router;
