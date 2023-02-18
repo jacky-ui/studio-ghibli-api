9117632ffc3744a2006cab98fa2da7c78b609513
jacky-ui <jacky.du.004@gmail.com>
2023-02-17 20:38:18 -0500

    fix typo in rateLimiter.js

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 const rateLimiterUsingThirdParty = rateLimit({
   windowMs: 15 * 60 * 60 * 1000, // 24 hrs in milliseconds
   max: 10,
-  message: 'You have exceeded the 100 requests in 24 hrs limit!', 
+  message: 'You have exceeded the 10 requests in 15 minute limit!', 
   standardHeaders: true,
   legacyHeaders: false,
 });
